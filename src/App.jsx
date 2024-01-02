e15e343da951bc2cfb27f43eafd0dd644527935c
sakshamgoel-github <saksham.goel0808@gmail.com>
2024-01-02 19:37:45 +0530

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import DropDown from "./components/DropDown";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>DropDown using Material UI</h1>
+      <DropDown />
+    </>
+  );
+}
+
+export default App;
